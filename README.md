b3ca88c991d01b708fc0f31bb9d8c98b01d286bf
Maxim <100160638+Wonnasmile@users.noreply.github.com>
2023-06-06 09:42:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sherif
+ Sherif balka
---
+++ Sherif/main.c
@@ -0,0 +1,50 @@
+/*
+ * Sherif.c
+ *
+ * Created: Пн 05.06.23 15:54:03
+ * Author : Max
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "avr/interrupt.h"
+
+#define QUANT 10
+
+#define T_IMPULSE 4 * QUANT
+#define T_PAUSE 4 * QUANT			// ms
+#define T_DELAY 20 * QUANT
+#define REPEATS 10
+
+
+int main(void)
+{
+  
+    DDRB =  0b00000101;  // PB0 - Output CTRL1, PB2 - Output CTRL2
+    PORTB = 0b00000101;  // PB0 = 0, PB1 = 0
+
+  
+	/* Replace with your application code */
+    while (1) 
+    {
+		for(uint8_t n = 0; n< REPEATS; n++)
+		{
+			PORTB = 0b00000001;
+			_delay_ms(T_IMPULSE);
+			PORTB = 0;
+			_delay_ms(T_PAUSE);
+		}
+		_delay_ms(T_DELAY);
+		
+		for(uint8_t n = 0; n< REPEATS; n++)
+		{
+			PORTB = 0b00000100;
+			_delay_ms(T_IMPULSE);
+			PORTB = 0;
+			_delay_ms(T_PAUSE);
+		}
+		_delay_ms(T_DELAY);
+		
+    }
+}
+
